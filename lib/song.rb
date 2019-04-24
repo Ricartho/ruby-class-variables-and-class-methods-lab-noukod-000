22b3143e658a41358532bf072b2f8db4b6291fb7
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-24 02:45:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song  
+  attr_reader :name,:artist,:genre 
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@artists << artist
+    @@genres <<  genre
+  end 
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end 
+  
+  def self.genre_count
+    counted = {}
+    @@genres.each do |el|
+      counted[el] = 1 unless
+    end 
+    counted
+  end 
+end 
